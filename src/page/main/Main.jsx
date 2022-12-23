edf0c775a6a3eb65d4b3bc6d4e6fe16af2b525a9
minseung-gang <strause1@nate.com>
2022-12-23 21:49:02 +0900

    RENAME:메인페이지 이름 수정

--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -1,5 +1,13 @@
 import React from 'react';
 
+import Header from '../../components/common/Header';
+import MainBoardList from '../../components/features/mainboardlist/MainBoardList';
+
 export default function Main() {
-  return <div>Main</div>;
+  return (
+    <div>
+      <Header />
+      <MainBoardList />
+    </div>
+  );
 }
