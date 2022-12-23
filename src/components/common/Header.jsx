4e043e8f5bed0c142c2dbf50f20dafa32e53cafd
minseung-gang <strause1@nate.com>
2022-12-23 21:46:50 +0900

    DESIGN: 헤더 스타일컴포넌트 수정

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -5,10 +5,12 @@ import useModal from '../../hooks/useSignModal';
 
 const Header = () => {
   const [modal, onChangeModalHandler] = useModal();
+
   // 모달창 x 버튼 눌렀을 때
   const closeEventHandler = () => {
     onChangeModalHandler();
   };
+
   const onSubmitHandler = () => {
     console.log('submit');
   };
@@ -20,17 +22,62 @@ const Header = () => {
         <span>ddd</span>
         <button type="button">ddd</button>
       </div>
-      <Modal modal={modal} close={closeEventHandler}>
-        <div>
-          <div>환영합니다</div>
-        </div>
-        <div>
-          <div>로그인</div>
-        </div>
+      <Modal
+        width="700px"
+        height="600px"
+        modal={modal}
+        close={closeEventHandler}
+      >
+        <StModalContent>
+          <div className="content_left">
+            <img
+              src="https://static.velog.io/static/media/undraw_joyride_hnno.fae6b95e.svg"
+              alt=""
+            />
+            <span className="content_title">환영합니다!</span>
+          </div>
+          <div className="content_right">
+            <div>로그인내용적어주기!</div>
+          </div>
+        </StModalContent>
       </Modal>
     </StHeader>
   );
 };
 export default Header;
 
-const StHeader = styled.div``;
+const StHeader = styled.div`
+  display: flex;
+`;
+const StModalContent = styled.div`
+  display: flex;
+  width: 100%;
+  height: inherit;
+  .content_left {
+    min-width: 220px;
+    max-width: 220px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    background: #1e1e1e;
+    gap: 10px 0;
+  }
+  .content_left,
+  .content_right {
+    padding: 1.5rem;
+    box-sizing: border-box;
+    color: #989898;
+  }
+  .content_title {
+    font-size: 1.7rem;
+    font-weight: 700;
+  }
+  img {
+    width: 100%;
+  }
+  .content_right {
+    width: 100%;
+    background: #121212;
+  }
+`;
