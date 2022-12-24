e274bf4fb806694be5fb75f52d06bdabefed7fd1
minseung-gang <strause1@nate.com>
2022-12-24 13:31:59 +0900

    FEAT: 로그인,회원가입 틀 수정 및 전환기능 추가

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,10 +1,15 @@
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { useState } from 'react';
+import { IoIosClose } from 'react-icons/io';
 import Modal from './Modal';
 import useModal from '../../hooks/useSignModal';
+import Signin from '../features/signin/Signin';
+import Signup from '../features/signup/Signup';
 
-const Header = () => {
+function Header() {
   const [modal, onChangeModalHandler] = useModal();
+  const [change, setChange] = useState(true);
 
   // 모달창 x 버튼 눌렀을 때
   const closeEventHandler = () => {
@@ -23,8 +28,8 @@ const Header = () => {
         <button type="button">ddd</button>
       </div>
       <Modal
-        width="700px"
-        height="600px"
+        width="600px"
+        height="550px"
         modal={modal}
         close={closeEventHandler}
       >
@@ -37,13 +42,35 @@ const Header = () => {
             <span className="content_title">환영합니다!</span>
           </div>
           <div className="content_right">
-            <div>로그인내용적어주기!</div>
+            <div className="modal_close">
+              <StClose />
+            </div>
+
+            <StTitle>{change ? '로그인' : '회원가입'}</StTitle>
+
+            <StContent>{change ? <Signin /> : <Signup />}</StContent>
+
+            {change ? (
+              <StBtnContainer>
+                아직 회원이 아니신가요?
+                <StChangeBtn onClick={() => setChange(!change)}>
+                  회원가입
+                </StChangeBtn>
+              </StBtnContainer>
+            ) : (
+              <StBtnContainer>
+                계정이 이미 있으신가요?
+                <StChangeBtn onClick={() => setChange(!change)}>
+                  로그인
+                </StChangeBtn>
+              </StBtnContainer>
+            )}
           </div>
         </StModalContent>
       </Modal>
     </StHeader>
   );
-};
+}
 export default Header;
 
 const StHeader = styled.div`
@@ -60,14 +87,22 @@ const StModalContent = styled.div`
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background: #1e1e1e;
+    background: #f8f9fa;
     gap: 10px 0;
   }
   .content_left,
   .content_right {
-    padding: 1.5rem;
+    padding: 1.8rem;
     box-sizing: border-box;
     color: #989898;
+  }
+  .content_right {
+    width: 100%;
+    background: #fff;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      display: none;
+    }
   }
   .content_title {
     font-size: 1.7rem;
@@ -76,8 +111,83 @@ const StModalContent = styled.div`
   img {
     width: 100%;
   }
-  .content_right {
+
+  .modal_close {
+    position: absolute;
+    right: 5px;
+    top: 5px;
+  }
+  h4 {
+    color: #868e96;
+    margin: 0.8rem 0;
+  }
+`;
+const StClose = styled(IoIosClose)`
+  font-size: 2rem;
+  color: #989898;
+  cursor: pointer;
+`;
+const StChangeBtn = styled.button`
+  color: #12b886;
+`;
+const StTitle = styled.h2`
+  font-size: 1.3rem;
+  font-weight: 600;
+  color: #000;
+`;
+const StContent = styled.div`
+  .flex-column {
+    display: flex;
+    flex-direction: column;
+    gap: 10px 0;
+  }
+  .form_field {
+    display: flex;
+    flex-direction: column;
+    gap: 5px 0;
+  }
+  .input_field_container {
+    height: 3rem;
+  }
+  input {
+    padding: 1rem;
+    box-sizing: border-box;
+    border-radius: 2px;
+    outline: none;
+    font-size: 1rem;
     width: 100%;
-    background: #121212;
+    flex: 1 1 0%;
+    border: 1px solid #dadada;
+  }
+  button {
+    background-color: #12b886;
+    border: none;
+    color: #fff;
+    font-size: 1rem;
+    font-weight: 700;
+    height: 3rem;
+    min-width: 6rem;
+    cursor: pointer;
+  }
+
+  .form_btn_container {
+    display: flex;
+    justify-content: flex-end;
+  }
+`;
+const StBtnContainer = styled.div`
+  margin-top: 5rem;
+  display: flex;
+  justify-content: flex-end;
+
+  button {
+    background: none;
+    border: none;
+    cursor: pointer;
+    font-size: 1rem;
+    font-weight: 700;
+    &:hover {
+      text-decoration: underline;
+    }
   }
 `;
