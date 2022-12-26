1c63533d08b5589505f372470f7b7a9c476cb23b
minseung-gang <strause1@nate.com>
2022-12-27 04:04:57 +0900

    DESIGN: 스타일컴포넌트 수정

--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -6,7 +6,6 @@ import MainBoardList from '../../components/features/mainboardlist/MainBoardList
 export default function Main() {
   return (
     <StMain>
-      <Header />
       <MainBoardList />
     </StMain>
   );
@@ -14,10 +13,4 @@ export default function Main() {
 
 const StMain = styled.div`
   margin: 0 auto;
-  @media (max-width: 1440px) {
-    width: 1024px;
-  }
-  @media (max-width: 1080px) {
-    width: calc(100% - 2rem);
-  }
 `;
