6f1b91c775f17eef9894d49e636b47a3f414ec77
minseung-gang <strause1@nate.com>
2022-12-27 03:30:05 +0900

    DESIGN: 반응형 작업 추가

--- src/page/main/Main.jsx
+++ src/page/main/Main.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
-
+import styled from 'styled-components';
 import Header from '../../components/common/Header';
 import MainBoardList from '../../components/features/mainboardlist/MainBoardList';
 
 export default function Main() {
   return (
-    <div>
+    <StMain>
       <Header />
       <MainBoardList />
-    </div>
+    </StMain>
   );
 }
+
+const StMain = styled.div`
+  margin: 0 auto;
+  @media (max-width: 1440px) {
+    width: 1024px;
+  }
+  @media (max-width: 1080px) {
+    width: calc(100% - 2rem);
+  }
+`;
