a707ff8aa4875d340069dc2dde5623f3fde5a78b
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-27 00:06:59 +0900

    로그인 기능 완료

--- src/components/features/signin/Signin.jsx
+++ src/components/features/signin/Signin.jsx
@@ -1,5 +1,8 @@
+import { useState } from 'react';
+import styled from 'styled-components';
 import { useSelector, useDispatch } from 'react-redux';
 import { setUserInfo } from '../../../redux/modules/userInfo';
+import { postLogin } from '../../../apis/auth';
 
 const TEMP_DATA = {
   nickName: 'ssori',
@@ -8,22 +11,51 @@ const TEMP_DATA = {
 };
 
 export default function Signin({ closeEventHandler }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const dispatch = useDispatch();
 
-  /* dispatch(actioncCreator) */
   const onSignInHandler = () => {
-    dispatch(setUserInfo(TEMP_DATA));
+    postLogin({
+      email,
+      password,
+    })
+      .then(res => {
+        localStorage.setItem('id', res.headers.authorization);
+        alert('로그인 완료');
+      })
+      .catch(error => console.log(error));
+    // TODO: 로그인이 성공시 profile정보를 받아오고 싶어
+    // 받아오면 redux에 넣어줄꺼야
+    // dispatch(setUserInfo(TEMP_DATA));
     closeEventHandler();
   };
+
+  const onEmailHandler = e => {
+    setEmail(e.target.value);
+  };
+
+  const onPasswordHandler = e => {
+    setPassword(e.target.value);
+  };
+
   return (
     <form action="" className="form_field">
       <h4>이메일</h4>
       <div className="flex-column">
         <StSignInContainer className="">
-          <input type="text" placeholder="이메일을 입력하세요" />
+          <input
+            type="text"
+            onChange={onEmailHandler}
+            placeholder="이메일을 입력하세요"
+          />
         </StSignInContainer>
         <StSignInContainer>
-          <input type="password" placeholder="비밀번호를 입력하세요" />
+          <input
+            type="password"
+            onChange={onPasswordHandler}
+            placeholder="비밀번호를 입력하세요"
+          />
         </StSignInContainer>
         <div className="form_btn_container">
           <button type="button" onClick={onSignInHandler}>
