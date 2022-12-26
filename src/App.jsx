43b12b791951ee9eb53c3124b0508329d972af34
minseung-gang <strause1@nate.com>
2022-12-27 04:13:31 +0900

    DOCS:문서 수정

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,9 @@ import Router from './router/Router';
 function App() {
   return (
     <div className="App">
-      <StMyPageContainer>
-        <Router />
-      </StMyPageContainer>
+      <Router />
     </div>
   );
 }
 
 export default App;
-
-const StMyPageContainer = styled.div`
-  /*  background-color: lightblue;
-  margin: 0 auto;
-    @media (min-width: 768px) {
-    width: 768px;
-  } */
-  margin: 0 auto;
-  @media (max-width: 1440px) {
-    width: 1024px;
-  }
-  @media (max-width: 1080px) {
-    width: calc(100% - 2rem);
-  }
-`;
