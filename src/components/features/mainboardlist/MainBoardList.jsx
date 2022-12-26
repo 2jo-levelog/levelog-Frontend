18f7605d0706cd33f7a3b7214726136c914e29c5
minseung-gang <strause1@nate.com>
2022-12-26 10:59:03 +0900

    DESIGN:메인페이지 스타일컴포넌트 수정

--- src/components/features/mainboardlist/MainBoardList.jsx
+++ src/components/features/mainboardlist/MainBoardList.jsx
@@ -1,5 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { authInstance, instance } from '../../../apis/axios';
+import Card from './Card';
 
 export default function MainBoardList() {
-  return <div>MainBoardList</div>;
+  const [boardList, setBoardList] = useState([]);
+
+  useEffect(() => {
+    instance.get('/api/main').then(response => {
+      setBoardList(response.data);
+    });
+  }, []);
+
+  return (
+    <StMainBord>
+      <StMainInner>
+        {boardList &&
+          boardList?.map(data => {
+            return <Card key={data.id} />;
+          })}
+      </StMainInner>
+    </StMainBord>
+  );
 }
+
+const StMainBord = styled.div`
+  max-width: 90%;
+  margin: 0 auto;
+`;
+const StMainInner = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  gap: 40px;
+`;
