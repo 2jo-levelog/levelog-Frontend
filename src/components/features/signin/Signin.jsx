5e1da881b81ac19a403948418ff9e7090ca98eac
minseung-gang <strause1@nate.com>
2022-12-26 11:02:06 +0900

    FEAT:중복확인 버튼 기능 추가

--- src/components/features/signin/Signin.jsx
+++ src/components/features/signin/Signin.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 export default function Signin() {
+  const submitHandler = async () => {
+    /*  await authInstance
+      .post('/api/auth/signIn', JSON.stringify({}))
+      .then(response => {
+        console.log(response);
+        if (response.success) {
+          console.log('회원가입 성공!');
+        } else if (response.status === 400) {
+          alert('이미 가입되어 있는 회원입니다.');
+        }
+      }); */
+  };
   return (
     <form action="" className="form_field">
       <h4>이메일</h4>
@@ -13,7 +26,9 @@ export default function Signin() {
           <input type="password" placeholder="비밀번호를 입력하세요" />
         </StSignInContainer>
         <div className="form_btn_container">
-          <button type="button">로그인</button>
+          <button type="button" onClick={submitHandler}>
+            로그인
+          </button>
         </div>
       </div>
     </form>
