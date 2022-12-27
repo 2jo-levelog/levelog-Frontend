512d9e4488fa72b4bf42c7a75921f9b46bdd373d
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-27 21:53:31 +0900

    Editor 개발

---
+++ src/page/detail/ToastEditor.jsx
@@ -0,0 +1,26 @@
+import { Editor } from '@toast-ui/react-editor';
+
+export default function ToastEditor(props) {
+  const { content, editorRef } = props;
+
+  return (
+    <Editor
+      // placeholder="내용을 입력해주세요."
+      initialValue={content || ' '}
+      ref={editorRef} // DOM 선택용 useRef
+      previewStyle="vertical" // 미리보기 스타일 지정
+      height="500px" // 에디터 창 높이
+      initialEditType="markdown" // 초기 입력모드 설정(디폴트 markdown)
+      theme="dark"
+      toolbarItems={[
+        // 툴바 옵션 설정
+        ['heading', 'bold', 'italic', 'strike'],
+        ['hr', 'quote'],
+        ['ul', 'ol', 'task', 'indent', 'outdent'],
+        // ['table', 'image', 'link'], 이미지 업로드 기능이 없어서 주석처리
+        ['image'],
+        ['code', 'codeblock'],
+      ]}
+    />
+  );
+}
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from '../components/common/Header';
+
 import Main from '../page/main/Main';
 import Mypage from '../page/mypage/Mypage';
+import DetailView from '../page/detail/EditView';
+import EditView from '../page/detail/DetailView';
 
 export default function Router() {
   return (
@@ -11,6 +14,8 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/mypage" element={<Mypage />} />
+        <Route path="/detail/" element={<DetailView />} />
+        <Route path="/edit" element={<EditView />} />
       </Routes>
     </BrowserRouter>
   );
