415485764ce4081a719ef968d1ec47fa20fa7148
minseung-gang <strause1@nate.com>
2022-12-28 01:29:49 +0900

    DESIGN: 스타일 컴포넌트 수정

--- src/components/features/mainboardlist/MainBoardList.jsx
+++ src/components/features/mainboardlist/MainBoardList.jsx
@@ -44,7 +44,7 @@ const StMainInner = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
   gap: 2rem;
-
+  margin: 0 0 1rem 0;
   @media (max-width: 1440px) {
     grid-template-columns: 1fr 1fr 1fr;
   }
