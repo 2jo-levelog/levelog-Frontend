c297aa1bbf528cafd522c8be48d45c21663419bc
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-27 21:49:24 +0900

    필요 없는 코드 정리

--- src/page/mypage/Mypage.jsx
+++ src/page/mypage/Mypage.jsx
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 import MyBoardList from '../../components/features/myboardlist/MyBoardList';
 import Profile from '../../components/features/myboardlist/Profile';
 import Header from '../../components/common/Header';
-import TestApi from '../../components/common/TestApi';
 
 export default function Mypage() {
   return (
@@ -12,7 +11,6 @@ export default function Mypage() {
       <StMyPageContainer>
         <ProfileWrapper>
           <Profile />
-          <TestApi />
         </ProfileWrapper>
         <StMyPageWrapper>
           <MyBoardList />
