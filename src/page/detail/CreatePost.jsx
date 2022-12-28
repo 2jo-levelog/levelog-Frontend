33406950f424150f392b2580c536885cdb7e9384
ssori <115159126+ssori0421@users.noreply.github.com>
2022-12-29 01:14:32 +0900

    createpage 수정중

--- src/page/detail/CreatePost.jsx
+++ src/page/detail/CreatePost.jsx
@@ -1,5 +1,7 @@
+import styled from 'styled-components';
 import { useState, useRef } from 'react';
 import { Button } from '@mui/material';
+import { BsArrowLeft } from 'react-icons/bs';
 import { createPost } from '../../apis/board';
 
 // Toast 에디터
@@ -30,17 +32,98 @@ export default function CreatePost({ type = 'edit' }) {
     createPost(formData);
   };
 
+  const onChangeTitleHandler = e => {
+    setTitle(e.target.value);
+  };
+
   return (
-    <div>
-      <input
+    <StMyPageContainer>
+      <StTextarea
         placeholder="타이틀을 입력해주세요"
         value={title}
-        onChange={e => {
-          setTitle(e.target.value); // TODO: onchangehandler 작성하기
-        }}
+        onChange={onChangeTitleHandler}
       />
       <ToastEditor editorRef={editorRef} />
-      <Button onClick={onSubmit}>제출하기</Button>
-    </div>
+      <STButtonsWrap>
+        <STBackButton onClick={onSubmit}>
+          <StArrowWrap>
+            <StArrow />
+            <StArrowText>나가기</StArrowText>
+          </StArrowWrap>
+        </STBackButton>
+
+        <STSubmitButton onClick={onSubmit}>제출하기</STSubmitButton>
+      </STButtonsWrap>
+    </StMyPageContainer>
   );
 }
+
+const StTextarea = styled.textarea`
+  background: transparent;
+  display: block;
+  padding: 0px;
+  font-size: 2.75rem;
+  width: 100%;
+  resize: none;
+  line-height: 1.5;
+  outline: none;
+  border: none;
+  font-weight: bold;
+  color: #212529;
+`;
+const STButtonsWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 0px;
+  margin: 8px 8px;
+`;
+
+const STBackButton = styled.button`
+  font-size: 1.124rem;
+  height: 2.5rem;
+  padding: 0.5rem 1rem;
+  -webkit-box-align: center;
+  align-items: center;
+  background: none;
+  border-radius: 4px;
+  cursor: pointer;
+  border: none;
+  display: flex;
+  outline: none;
+  color: var(--text1);
+`;
+const StArrowWrap = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const StArrow = styled(BsArrowLeft)`
+  font-size: 20px;
+`;
+const StArrowText = styled.span`
+  margin-left: 8px;
+  font-size: 1.125rem;
+`;
+const STSubmitButton = styled.button`
+  align-items: center;
+  justify-content: center;
+  font-weight: bold;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  background-color: #12b886;
+  color: #ffffff;
+  border-radius: 4px;
+  padding: 0px 1.25rem;
+  height: 2.5rem;
+  font-size: 1.125rem;
+`;
+
+const StMyPageContainer = styled.div`
+  /* background-color: lightblue; */
+  margin: 0 auto;
+  @media (min-width: 768px) {
+    width: 768px;
+  }
+`;
